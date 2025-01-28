d008d4ff1fe85ff52f828875d864cf9afedb20c5
Amit Raj <rajamitnine@gmail.com>
2025-01-28 19:23:05 +0530

    Create a Javascript file using vi commands and open it in the vi editor modify, save and exit using the same

---
+++ git_and_github/delete.js
@@ -0,0 +1,8 @@
+// A file created just for understanding the working of VI Text Editor using "vi commands" :
+
+const myName = "Amit";
+console.log(myName);
+console.log(typeof myName);
+const yourName = new String("Amit");
+console.log(yourName, typeof yourName);
+console.log(myName === yourName);
