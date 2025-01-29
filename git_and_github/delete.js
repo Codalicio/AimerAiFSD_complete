ee496a2dfe4c3eac6a12bc7ef29eff70c539188d
Amit Raj <rajamitnine@gmail.com>
2025-01-30 00:03:33 +0530

    Delete unwanted from Local Repo

--- git_and_github/delete.js
+++
@@ -1,8 +0,0 @@
-// A file created just for understanding the working of VI Text Editor using "vi commands" :
-
-const myName = "Amit";
-console.log(myName);
-console.log(typeof myName);
-const yourName = new String("Amit");
-console.log(yourName, typeof yourName);
-console.log(myName === yourName);
